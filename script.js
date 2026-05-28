b11f14b76996ed5ea227b5811e83880f357748f4
bidodeta <95609154+bidodeta@users.noreply.github.com>
2026-05-28 15:45:01 +0700

    Add files via upload
    Add 3 files

---
+++ script.js
@@ -0,0 +1,38 @@
+function compute() {
+  const principal = parseInt(document.getElementById("principal").value);
+  const rate = parseFloat(document.getElementById("rate").value);
+  const years = parseInt(document.getElementById("years").value);
+  var year = new Date().getFullYear() + parseInt(years);
+  const interest = (principal * years * rate) / 100;
+  const amount = principal + interest;
+  const result = document.getElementById("result");
+  if (principal <= 0) {
+    alert("Enter a positive number");
+    document.getElementById("principal").focus();
+  } else {
+    result.innerHTML = result.innerHTML =
+      "If you deposit $" +
+      "<mark>" +
+      principal +
+      "</mark>" +
+      ",\<br\> at an interest rate of " +
+      "<mark>" +
+      rate +
+      "%" +
+      "</mark>" +
+      "\<br\> You will receive an amount of $" +
+      "<mark>" +
+      amount +
+      "</mark>" +
+      ",\<br\> in the year " +
+      "<mark>" +
+      year +
+      "</mark>" +
+      "\<br\>";
+  }
+}
+//Update the display of the interest rate
+function updateRate() {
+  const rateval = document.getElementById("rate").value;
+  document.getElementById("rate_val").innerText = rateval;
+}
